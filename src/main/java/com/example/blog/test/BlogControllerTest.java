2c4684fda66e8610382dcc4ca38385d7c4fca6c7
Eziosan <nmkl96@naver.com>
2024-03-18 00:00:47 +0900

    환경세팅 완료

---
+++ src/main/java/com/example/blog/test/BlogControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.blog.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@RestController 
+public class BlogControllerTest {
+	
+	//http://localhost:8080/test/hello
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+}
